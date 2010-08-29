1db19a343e23fedcba5675aee69c9ce81e0e2f68
mrb <mrburke@gmail.com>
2010-08-29 16:35:43 -0300

    This sounds pretty incredible

--- src/com/snobwall/nesslet/NoteRunner.java
+++ src/com/snobwall/nesslet/NoteRunner.java
@@ -6,81 +6,108 @@ public class NoteRunner
 	
 	protected int _noteLen = 100; // milliseconds
 	protected int _tickLen;
+	protected int _numTicks = 3; // ticks per row (ie. per note change)
 	
 	public NoteRunner(int frequency)
 	{
 		_frequency = frequency;
 		
-		_tickLen = _frequency * _noteLen / 1000;
+		_tickLen = _frequency * _noteLen / 1000 / _numTicks;
 		
 		sqwave = new SquareWaveGen(frequency, 50);
-		sqwave.set_frequency((int)(_notes[_noteOffs] / 2));
-		sqwave.set_dutyCycle(_duties[_noteOffs]);
 		
 		sqwave.set_amplitude((byte)60);
 	}
 	
 	protected SquareWaveGen sqwave;
 	
-	protected int _tickOffs = 0;
-	protected int _noteOffs = 0;
-	protected int[] _notes = {
-			523, 659, 784,
-			523, 659, 784,
-			523, 659, 784,
-			698, 880, 1046,
-			698, 880, 1046,
-			698, 880, 1046,
-			784, 988, 1175,
-			784, 988, 1175,
-			784, 988, 1175,
-		};
-	protected int[] _duties = {50, 50, 50};
-	
-	
-	public byte[] getSamples_8(int count)
+	protected class Note
 	{
-		byte[] buf = new byte[count];
-		return getSamplesR_8(buf, 0, count);
+		// -1 for unset for all fields
+		public byte noteNum;
+		public byte vol; // 0 .. 127
+		public byte duty;
+		public byte vibrato;
+		
+		public Note(byte noteNum, byte vol, byte duty, byte vibrato) {
+			super();
+			this.noteNum = noteNum;
+			this.vol = vol;
+			this.duty = duty;
+			this.vibrato = vibrato;
+		}
+		
 	}
 	
-	public byte[] getSamplesR_8(byte[] buf, int writeOffs, int count)
+	static int frequencyOf(byte note)
 	{
-		int copied = 0;
+		return (int)(440.0 * Math.pow(Math.pow(2.0, (1.0/12.0)), (double)(note - 49)));
+	}
+
+	protected Note _notes[] =
+	{
+		new Note((byte)47, (byte)64, (byte)50, (byte)0),
+		new Note((byte)-1, (byte)48, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)64, (byte)-1, (byte)-1),
+		new Note((byte)40, (byte)-1, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)16, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)32, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)48, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)64, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)80, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)12, (byte)-1, (byte)-1),
+		new Note((byte)42, (byte)96, (byte)-1, (byte)-1),
+		new Note((byte)-1, (byte)12, (byte)-1, (byte)-1),
+		new Note((byte)44, (byte)96, (byte)31, (byte)-1),
+		new Note((byte)-1, (byte)12, (byte)-1, (byte)-1),
+		new Note((byte)45, (byte)96, (byte)10, (byte)-1),
+		new Note((byte)-1, (byte)12, (byte)-1, (byte)-1),
+	};
+	
+	// State!
+	protected Note _state_note;
+	protected int _state_frameOffs = 0; // which frame, inside the tick, are we on?
+	protected int _state_tickOffs = 0; // which tick are we on?
+	protected int _state_noteOffs = 0; // which note are we playing?
+
+	protected byte nextSample()
+	{
+		byte sample;
 		
-		do
+		if (_state_tickOffs == 0 && 
+				_state_frameOffs == 0) 
 		{
-			int numGen = _tickLen - _tickOffs;
-			if (numGen > count - copied)
+			Note newNote = _notes[_state_noteOffs];
+			
+			// new note; change things as appropriate
+			if (newNote.noteNum >= 0)
 			{
-				numGen = count - copied;
+				sqwave.set_frequency(frequencyOf(newNote.noteNum));
 			}
-			
-			sqwave.getSamplesR_8(buf, writeOffs + copied, numGen);
-			
-			copied += numGen;
-			_tickOffs += numGen;
-			
-			if (_tickOffs >= _tickLen)
+			if (newNote.vol >= 0)
 			{
-				_tickOffs = 0;
-				_noteOffs = (_noteOffs + 1) % _notes.length;
-				sqwave.set_frequency((int)(_notes[_noteOffs] / 2));
-				
-				if (_noteOffs == 0)
-				{
-					int dc = sqwave.get_dutyCycle();
-					dc += 10;
-					if (dc > 50)
-					{
-						dc = 10;
-					}
-					sqwave.set_dutyCycle(dc);
-				}
-				
+				sqwave.set_amplitude(newNote.vol);
 			}
-		} while(copied < count);
+			if (newNote.duty >= 0)
+			{
+				sqwave.set_dutyCycle(newNote.duty);
+			}
+		}
 		
-		return buf;
+		sample = sqwave.nextSample();
+		
+		// prepare for next frame
+		_state_frameOffs = (_state_frameOffs + 1) % _tickLen;
+		if (_state_frameOffs == 0)
+		{
+			_state_tickOffs = (_state_tickOffs + 1) % _numTicks;
+			if (_state_tickOffs == 0)
+			{
+				_state_noteOffs = (_state_noteOffs + 1) % _notes.length;
+				// if _state_noteOffs is 0, then we've looped the song
+			}
+		}
+		
+		return sample;
 	}
 }
