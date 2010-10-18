9b67a170f7ec1adddfede239de64595d7d73e85b
mrb <mrburke@gmail.com>
2010-10-17 22:02:34 -0300

    Moved playback into a service.

---
+++ src/com/snobwall/nesslet/SvcAudioPlayer.java
@@ -0,0 +1,84 @@
+package com.snobwall.nesslet;
+
+import java.util.Random;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class SvcAudioPlayer extends Service {
+
+	//
+	// Local binding-related stuff
+	//
+	
+    public class LocalBinder extends Binder {
+        SvcAudioPlayer getService() {
+            return SvcAudioPlayer.this;
+        }
+    }
+
+    private final IBinder _binder = new LocalBinder();
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return _binder;
+	}
+	
+	//
+	// Service overrides
+	//
+	
+    @Override
+    public void onCreate()
+    {
+    	System.err.println("SvcAudioPlayer onCreate");
+    }
+	
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+    	System.err.println("SvcAudioPlayer onStart");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+    	System.err.println("SvcAudioPlayer onDestroy");
+    }
+    
+    //
+    // Everything else
+    //
+    
+    protected Player player = null;
+	
+    public void stopPlayback()
+    {
+    	if (player != null)
+    	{
+	    	player.stopPlayback();
+	    	player = null;
+    	}
+    }
+    
+    public void startPlayback()
+    {
+    	player = new Player();
+    	player.startPlayback();
+    }
+    
+    protected int aNumber = 0;
+    
+    public int getANumber()
+    {
+    	if (aNumber == 0) 
+    	{
+    		aNumber = new Random().nextInt();
+    	}
+    	
+    	return aNumber;
+    }
+}
+
--- src/com/snobwall/nesslet/TestEditor.java
+++ src/com/snobwall/nesslet/TestEditor.java
@@ -1,16 +1,50 @@
 package com.snobwall.nesslet;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class TestEditor extends Activity {
+	
+    protected SvcAudioPlayer _player = null;
+    
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.testeditor);
         
+        System.err.println("TestEditor.onCreate: bindService = " + bindService(new Intent(this, SvcAudioPlayer.class),
+    			_svcConnection, Context.BIND_AUTO_CREATE));
         
     }
+    
+    public void onPlayerReady()
+    {
+        if (_player == null)
+        {
+        	System.err.println("Player is null!");
+        }
+        else
+        {
+        	System.err.println("TestEditor.onCreate: Service's number is " + _player.getANumber());
+        }
+    }
+
+    private ServiceConnection _svcConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            _player = ((SvcAudioPlayer.LocalBinder)service).getService();
+        	System.err.println("TestEditor._svcConnection is getting a service (" + _player + ").");
+        	onPlayerReady();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            _player = null;
+        }
+    };
 
 }
